e0aa1b7d207f87ab3c77287c87b9abf7d06fe921
maxine-jay <maxinejaysimpson@gmail.com>
2023-07-18 15:30:56 +0100

    Ex 3.4

--- partThree/index.js
+++ partThree/index.js
@@ -43,7 +43,7 @@ app.get("/info", (req, res) => {
 
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
+  const person = persons.find((p) => p.id === id);
   if (person) {
     res.json(person);
   } else {
@@ -51,6 +51,12 @@ app.get("/api/persons/:id", (req, res) => {
   }
   res.json(person);
 });
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter((p) => p.id !== id)
+    res.status(204).end();
+})
 
 const PORT = 3001;
 app.listen(PORT);
