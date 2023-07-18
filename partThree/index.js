c8496c1243ca634199ead66616185fd521ce97c5
maxine-jay <maxinejaysimpson@gmail.com>
2023-07-18 17:25:59 +0100

    Ex 3.6

--- partThree/index.js
+++ partThree/index.js
@@ -52,18 +52,44 @@ app.get("/api/persons/:id", (req, res) => {
   res.json(person);
 });
 
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+  return maxId;
+};
 app.post("/api/persons/", (req, res) => {
-    const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-    const person = req.body;
-    person.id = maxId + 1;
-    res.json(person)
-})
+  const person = req.body;
+  person.id = generateId();
+  let nameMatch = persons.find((p) => p.name === person.name) ? true : false;
+
+  if (person.name.length === 0) {
+    return res.status(400).json({
+      error: "Name missing",
+    });
+  } else if (person.number.length === 0) {
+    return res.status(400).json({
+      error: "Number missing",
+    });
+  } else if(nameMatch) {
+    return res.status(400).json({
+        error: "Name must be unique. A contact with this name already exists in the phonebook",
+      });
+  }
+
+  const newPerson = {
+    name: person.name,
+    number: person.number,
+    id: person.id,
+  };
+
+  persons = persons.concat(newPerson);
+  res.json(newPerson);
+});
 
 app.delete("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter((p) => p.id !== id)
-    res.status(204).end();
-})
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
+});
 
 const PORT = 3001;
 app.listen(PORT);
